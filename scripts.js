e1533186396bd9f1e12526387b1f9f48f7322127
Motta <125099026+Mottaqbota@users.noreply.github.com>
2024-05-27 17:39:51 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
             speed: 100,
             loop: true
         })
+        .type("Olá, meu nome é Motta!", {delay: 1000}).delete(22)
         .type("su eou", {delay: 400})
         .move(-5, {speed: 1500})
         .delete(1)
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .type(" Desenvolvedor Front-End!", {delay: 1600}).delete(24)
         .type("Designer ", {delay: 200} )
         .type("UI/UX!", {delay: 1600}).delete(22)
-        .type("A caminho do Full-Stack!!", {delay: 2000}).delete(40)
+        .type("A caminho do Full-Stack!", {delay: 2000}).delete(40)
         // .type("E Posso ser seu Programador!", {delay: 2000}).delete(28)
         .go()
     }, 2000)
